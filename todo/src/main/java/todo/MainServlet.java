6e599ef58e9d73814d13a43f31ce4f7fe51becb4
Wooyongjeong <dndyd1231@naver.com>
2020-12-24 16:03:52 +0900

    Create MainServlet.java
    * http://localhost:8000/todo/ url에 대응되는 MainServlet 생성하였습니다.

---
+++ todo/src/main/java/todo/MainServlet.java
@@ -0,0 +1,35 @@
+package todo;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class MainServlet
+ */
+@WebServlet("/")
+public class MainServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public MainServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>hello world!</h1>");
+		out.close();
+	}
+
+}
