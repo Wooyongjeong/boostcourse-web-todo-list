62bc23a9e28a26072ed394b04aacdaa28055c1b9
Wooyongjeong <dndyd1231@naver.com>
2020-12-26 02:37:50 +0900

    Create TodoTypeServlet.java
    * main.jsp에서 ➡ 버튼을 클릭하였을 때에 대응되는 서블릿인 TodoTypeServlet.java를 추가하였습니다.
    
    * todo 테이블에서 해당하는 id값에 대응되는 항목의 type을 바꾸어주고, 성공했다면 상태코드 200을 반환해줍니다.

---
+++ todo/src/main/java/todo/TodoTypeServlet.java
@@ -0,0 +1,45 @@
+package todo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import todo.dao.TodoDao;
+
+/**
+ * Servlet implementation class TodoServlet
+ */
+@WebServlet("/type")
+public class TodoTypeServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodoTypeServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String idStr = request.getParameter("id");
+		String type = request.getParameter("type");
+		System.out.println(idStr + ", " + type);
+		long id = Long.parseLong(idStr);
+		TodoDao todoDao = new TodoDao();
+		int updateCount = todoDao.updateTodo(id, type);
+		if (updateCount == 1) {
+			response.setStatus(200);
+		}
+		else {
+			response.sendError(400, "해당하는 ID값이 없습니다.");
+		}
+	}
+
+}
